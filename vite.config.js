b7224f6f99841ebbf70395d14fe7cab8caa42f2d
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2022-12-22 14:58:20 +0530

    Svelte Js Project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/Svelte-js-Project/",
+  base: "/Svelte-js-Project/",
   plugins: [svelte()],
 })
